91166139c15fe614ca3fbdef7ed74f08c63f6b27
Norton <yuuki_vs_ninjin@outlook.jp>
2016-11-29 20:19:13 +0900

    [変更] 無意味なコメントを削除

--- CreationOfDungeon_master/CharaBase.h
+++ CreationOfDungeon_master/CharaBase.h
@@ -13,8 +13,6 @@ public:
         chara_num = _chara_num;
     }
 
-    //  template<class Chara>
-    //  CharaBase(const Chara& target): : p_(new HeroHolder<Chara>(target)) {}
     virtual ~CharaBase();
 
     virtual void Update() = 0;
@@ -27,31 +25,6 @@ public:
 private:
     std::string chara_num;
 
-    /*
-    class CharaTypeBase {
-    public:
-        ~CharaTypeBase() {}
-    };
-
-    typedef std::tr1::shared_ptr<CharaTypeBase> holder_ptr;
-
-
-    template<class Chara>
-    class CharaDerived : public CharaTypeBase
-    {
-    public:
-        CharaDerived(Chara const &target) :_target(target) {}
-    private:
-        Chara _target;
-    };
-
-    holder_ptr p_;
-
-    /*
-    https://mmiyano.blogspot.jp/2012/10/blog-post.html 参考
-    また作り直すん・・・・？ﾌﾞﾜｯ
-    HeroBaseのErasureTypeをCharaBaseに移動させる
-    */
     
 };
 
