d3ddc1d048fd30e03b299e4097d31bdb78e474da
negative <pchsp33mespos@ezweb.ne.jp>
2016-12-26 18:34:18 +0900

    ゲームシーンでのゲームオーバー、ステージクリア、ゲームクリアの演出の元（画面がだんだんと暗転する部分）について追加 ゲーム画面で左クリックしたらゲームオーバーフラグが立つようになってます。

--- CreationOfDungeon_master/Game.cpp
+++ CreationOfDungeon_master/Game.cpp
@@ -1,5 +1,8 @@
 #include "Game.h"
 #include "MouseManager.h"
+#include "Title.h"
+#include "DxLib.h"
+
 
 Game::Game()
     :stage_num("1-1"),
@@ -8,6 +11,12 @@ Game::Game()
 
 
     chara_manager = CharaManager(stage_num);
+
+
+    //ここから追加 by negative
+    gameOverCount = 0;
+    isGameOver = false;
+    
 }
 
 
@@ -20,6 +29,52 @@ SceneBase * Game::Update()
 
     mouse_status = MouseManager::GetMouseStatus(mouse_status);
     
+    if (mouse_status._mouse_left_click) {
+        if (isGameOver == false && isStageClear == false && isGameClear == false) {
+            isGameOver = true;
+        }
+    }
+    if (mouse_status._mouse_right_click) {
+        if (isGameOver == false && isStageClear == false && isGameClear == false) {
+            isStageClear = true;
+        }
+    }
+    if (mouse_status._mouse_middle_click) {
+        if (isGameOver == false && isStageClear == false && isGameClear == false) {
+            isGameClear = true;
+        }
+    }
+
+    if (isGameOver == true) {
+        bool goTitle = GameOverUpdate();
+        if (goTitle == true) {
+            return new Title();
+        }
+        else {
+            return this;
+        }
+    }
+
+    if (isStageClear == true) {
+        bool goTitle = StageClearUpdate();
+        if (goTitle == true) {
+            return new Title();
+        }
+        else {
+            return this;
+        }
+    }
+
+    if (isGameClear == true) {
+        bool goTitle = GameClearUpdate();
+        if (goTitle == true) {
+            return new Title();
+        }
+        else {
+            return this;
+        }
+    }
+
     chara_manager.Update(mouse_status);
 
     map_process_manager.Update(mouse_status);
@@ -29,7 +84,142 @@ SceneBase * Game::Update()
 
 void Game::Draw()
 {
+
+
+    DrawBox(0, 0, 1024, 768, GetColor(255, 255, 255), TRUE);
+    DrawBox(0, 0, 124, 268, GetColor(255, 0, 255), TRUE);
+    DrawBox(300, 288, 524, 548, GetColor(255, 255, 0), TRUE);
+
+    
+
+
     chara_manager.Draw();
 
     map_process_manager.Draw();
+
+
+    if (isGameOver == true) {
+
+        GameOverDraw();
+
+    }
+    if (isStageClear == true) {
+
+        StageClearDraw();
+
+    }
+    if (isGameClear == true) {
+
+        GameClearDraw();
+
+    }
+
 }
+
+
+bool Game::GameOverUpdate() {
+
+    
+    gameOverCount++;
+
+    if (gameOverCount > 200 && mouse_status._mouse_left_click) {
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+void Game::GameOverDraw() {
+
+    double blend = double(gameOverCount)*17/10;
+
+    if (blend > 150) {
+        blend = 150;
+    }
+
+    SetDrawBlendMode(DX_BLENDMODE_ALPHA, blend);
+    DrawBox(0, 0, 1024, 768, GetColor(0, 0, 0), TRUE);
+    SetDrawBlendMode(DX_BLENDMODE_ALPHA, 255);
+    
+    if (gameOverCount > 150) {
+        DrawFormatString(200, 200, GetColor(255, 0, 0), "ガメオベラ");
+    }
+    if (gameOverCount > 200) {
+        DrawFormatString(200, 400, GetColor(255, 255, 255), "左クリックで戻る");
+    }
+
+}
+
+bool Game::StageClearUpdate() {
+
+    StageClearCount++;
+
+    if (StageClearCount > 200 && mouse_status._mouse_left_click) {
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+void Game::StageClearDraw() {
+
+    double blend = double(StageClearCount) * 17 / 10;
+
+    if (blend > 150) {
+        blend = 150;
+    }
+
+    SetDrawBlendMode(DX_BLENDMODE_ALPHA, blend);
+    DrawBox(0, 0, 1024, 768, GetColor(0, 0, 0), TRUE);
+    SetDrawBlendMode(DX_BLENDMODE_ALPHA, 255);
+
+    if (StageClearCount > 150) {
+        DrawFormatString(200, 200, GetColor(255, 0, 0), "ステージクリア");
+    }
+    if (StageClearCount > 200) {
+        DrawFormatString(200, 400, GetColor(255, 255, 255), "左クリックで戻る");
+    }
+
+}
+
+bool Game::GameClearUpdate() {
+
+    GameClearCount++;
+
+    if (GameClearCount > 200 && mouse_status._mouse_left_click) {
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+void Game::GameClearDraw() {
+
+    double blend = double(GameClearCount) * 17 / 10;
+
+    if (blend > 150) {
+        blend = 150;
+    }
+
+    SetDrawBlendMode(DX_BLENDMODE_ALPHA, blend);
+    DrawBox(0, 0, 1024, 768, GetColor(0, 0, 0), TRUE);
+    SetDrawBlendMode(DX_BLENDMODE_ALPHA, 255);
+
+    if (GameClearCount > 150) {
+        DrawFormatString(200, 200, GetColor(255, 0, 0), "ゲームクリア");
+    }
+    if (GameClearCount > 200) {
+        DrawFormatString(200, 400, GetColor(255, 255, 255), "左クリックで戻る");
+    }
+
+}
+
--- CreationOfDungeon_master/Game.h
+++ CreationOfDungeon_master/Game.h
@@ -23,5 +23,27 @@ private:
 
     MouseContent mouse_status;
 
+
+    //ここから下が追加した部分(negative)
+    bool isGameOver;    //デバッグとしてとりあえず追加。ゲームオーバーしているかどうか。trueでゲームオーバーしている
+    int gameOverCount;  //ゲームオーバーの処理を始めてから進んだフレーム数
+
+    bool GameOverUpdate();
+    void GameOverDraw();
+
+    //ここから下が追加した部分(negative)
+    bool isStageClear;    //デバッグとしてとりあえず追加。ステージをクリアしているかどうか。trueでステージをクリアしている
+    int StageClearCount;  //ステージのクリアの処理を始めてから進んだフレーム数
+
+    bool StageClearUpdate();
+    void StageClearDraw();
+
+    //ここから下が追加した部分(negative)
+    bool isGameClear;    //デバッグとしてとりあえず追加。ゲームをクリアしているかどうか。trueでゲームをクリアしている
+    int GameClearCount;  //ゲームのクリアの処理を始めてから進んだフレーム数
+
+    bool GameClearUpdate();
+    void GameClearDraw();
+
 };
 
