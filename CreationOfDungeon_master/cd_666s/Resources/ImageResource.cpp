7e928c876719604e4739128419aa1fac62b20afd
666-2bytemoji <1k9k1n@gmail.com>
2017-01-09 18:52:47 +0900

    [変更]ImageResourceのidカウントはDxLibでは不要

--- CreationOfDungeon_master/cd_666s/Resources/ImageResource.cpp
+++ CreationOfDungeon_master/cd_666s/Resources/ImageResource.cpp
@@ -6,9 +6,6 @@ ImageResource::ImageResource(std::string fileName)
     : Resource(fileName)
     , _handle(NULL)
 {
-    static int totalID = 1;
-    totalID++;
-
     _handle = LoadGraph(fileName.c_str());
 
     int iWidth = 0;
