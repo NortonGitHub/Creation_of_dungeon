6b0a2b5573bf274849f752c9a7ab9f55d9733d7e
Norton <yuuki_vs_ninjin@outlook.jp>
2017-02-13 13:38:46 +0900

    [追加] MakeDungeonクラスを追加

---
+++ CreationOfDungeon_master/MakeDungeon.cpp
@@ -0,0 +1,22 @@
+#include "MakeDungeon.h"
+
+
+
+MakeDungeon::MakeDungeon()
+{
+}
+
+
+MakeDungeon::~MakeDungeon()
+{
+}
+
+void MakeDungeon::Update()
+{
+    if (MOUSE->ButtonDown(MouseInput::MouseButtonCode::MOUSE_L)){
+    }
+}
+
+void MakeDungeon::Draw()
+{
+}
---
+++ CreationOfDungeon_master/MakeDungeon.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "cd_666s\InputManager\MouseInput.h"
+class MakeDungeon
+{
+public:
+    MakeDungeon();
+    ~MakeDungeon();
+
+    void Update();
+
+    void Draw();
+};
+
