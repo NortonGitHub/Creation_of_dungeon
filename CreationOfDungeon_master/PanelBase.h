ca3262580879a00563c68cc4dafc52ba79a5ffc5
Norton <yuuki_vs_ninjin@outlook.jp>
2017-03-20 19:14:39 +0900

    [編集] 仮想関数のうち2つを追加

--- CreationOfDungeon_master/PanelBase.h
+++ CreationOfDungeon_master/PanelBase.h
@@ -1,12 +1,16 @@
 #pragma once
+#include "PanelContent.h"
 class PanelBase
 {
 public:
     PanelBase();
     ~PanelBase();
 
-    virtual void Update() {};
-    virtual void Draw() {};
+    virtual void Update();
+    virtual void Draw();
+
+    virtual std::string GetCategoryName();
+    virtual void SetObject(PanelBase _other_panel);
 
    // virtual void SetPanel
 };
