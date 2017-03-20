a46d28452655dd8498ecfb49e10722624d9f99ed
Norton <yuuki_vs_ninjin@outlook.jp>
2017-03-20 19:16:56 +0900

    [変更] 新しいUI管理システムを模索中(コメントアウト化)

--- CreationOfDungeon_master/EditMap.cpp
+++ CreationOfDungeon_master/EditMap.cpp
@@ -8,6 +8,9 @@
 #include "PanelSettingObject.h"
 
 
+#include <typeinfo>
+
+
 EditMap::EditMap(std::string _stage_num)
     : stage_num(_stage_num)
 {
@@ -34,7 +37,8 @@ SceneBase * EditMap::Update(UIManager _ui)
 
     std::string _ui_name;
 
-#if 1
+#if 0
+    /*試しコード*/
     /*NOTE*/
     /*
         PanelBaseには、そのパネルの名前、座標、変数が格納されている
@@ -42,11 +46,63 @@ SceneBase * EditMap::Update(UIManager _ui)
     */
     std::vector<PanelBase> panels; //試験コード用
 
-    for(auto p : panels){
+    /*
+        別のオブジェクトへのアクセスが必要になった際に
+        対象となるオブジェクトを格納する
+        配列 ならびに 簡易関数
+    */
+    /*multimap キー:カテゴリ名, 値:対象のクラス型オブジェクト*/
+    std::multimap<std::string, PanelBase> _targets;
+    auto SetClassType = [&](std::string _category) {
+        _targets.clear();
+        for (auto ps : panels) {
+            if (ps.GetCategoryName() == _category) {
+                _targets.emplace(_category, ps);
+            }
+        }
+    };
+    /*↑の処理
+    無意味じゃない？
+     -> 大本の配列panelsの中の、型がクラスPanelSettingObject
+     の要素のPanelContentを書き換えるのだから、
+     ただ消すだけでいい
+    */
+
+    for (auto p : panels) {
         //
         bool isClicked = p.IsClicked();
 
-        if(isClicked){
+        //何かのオブジェクトがクリック
+        //そのオブジェクトの種類(クラス)によって
+        //処理内容が変化する。
+        //また、別のサブクラスのオブジェクトに
+        //干渉する
+
+
+        //オブジェクトがクリックされたら
+        if (isClicked) {
+            auto str = std::string(typeid(p).name());
+
+            if (str.find("AffectObjects") != std::string::npos) {
+#if 0
+                std::map<std::string, PanelBase> panel_temps;//消すかも
+                std::string _category = p.GetCategoryName();
+
+                for (auto sp : panels) {
+                    std::string str_s = sp.GetCategoryName();
+                    if (str_s.find(_category) == std::string::npos) {
+                        panel_temps.insert(_category, sp);
+                    }
+                }
+#endif
+                SetClassType(p.GetCategoryName());
+
+                for (auto _obj : _targets) {
+
+                    p.SetObject(_obj.second);
+                }
+            }
+
             p.Update();
         }
     }
